b78947939ad5f9df8e721ddc9f4cf29ee25453a2
vaibhavbhatt1506 <vaibhav.23bey10081@vitbhopal.ac.in>
2026-02-01 23:25:56 +0530

    Add files via upload
    greetings code in functions

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+        String greeting = "How are you";
+        return greeting;
+    }
+}
